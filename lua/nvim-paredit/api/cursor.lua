b3014beb97a4e540c581241709c7000e1899e839
Artem Medeu <artem.medeusheyev@gmail.com>
2023-08-27 23:33:30 +0600

    fix: typo in cursor api + make it testable

--- lua/nvim-paredit/api/cursor.lua
+++ lua/nvim-paredit/api/cursor.lua
@@ -4,13 +4,14 @@ function M.insert_mode()
   vim.api.nvim_feedkeys("i", "n", true)
 end
 
-function M.place_cursor(range_or_node, opts)
+function M.get_cursor_pos(range_or_node, opts)
   local range
 
   if type(range_or_node) == "table" then
     range = range_or_node
   elseif type(range_or_node) == "userdata" then
     range = { range_or_node:range() }
+    range[4] = range[4] - 1
   end
 
   if not range then
@@ -22,15 +23,21 @@ function M.place_cursor(range_or_node, opts)
     cursor_pos = { range[1] + 1, range[2] }
   elseif opts.placement == "inner_start" then
     cursor_pos = { range[1] + 1, range[2] + 1 }
-  elseif opts.placement == "inned_end" then
-    cursor_pos = { range[3] + 1, range[4] - 1 }
-  else
+  elseif opts.placement == "inner_end" then
     cursor_pos = { range[3] + 1, range[4] }
+  else
+    cursor_pos = { range[3] + 1, range[4] + 1 }
   end
-  vim.api.nvim_win_set_cursor(0, cursor_pos)
+  return cursor_pos
+end
 
-  if opts.mode == "insert" then
-    M.insert_mode()
+function M.place_cursor(range_or_node, opts)
+  local cursor_pos = M.get_cursor_pos(range_or_node, opts)
+  if cursor_pos then
+    vim.api.nvim_win_set_cursor(0, cursor_pos)
+    if opts.mode == "insert" then
+      M.insert_mode()
+    end
   end
 end
 
