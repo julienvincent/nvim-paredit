e757bf61798d411605954e62cb2a95b2a1cba9ae
Oliver Caldwell <ollie@oli.me.uk>
2023-09-23 14:51:50 +0100

    Deduplicate deletions code, combine functions into shared, configurable local functions

--- lua/nvim-paredit/api/deletions.lua
+++ lua/nvim-paredit/api/deletions.lua
@@ -2,8 +2,7 @@ local selections = require("nvim-paredit.api.selections")
 
 local M = {}
 
-function M.delete_form()
-  local range = selections.get_range_around_form()
+local function delete_form_impl(range)
   if not range then
     return
   end
@@ -16,10 +15,17 @@ function M.delete_form()
     range[3], range[4],
     {}
   )
+end
+
+function M.delete_form()
+  delete_form_impl(selections.get_range_around_form())
 end
 
 function M.delete_top_level_form()
-  local range = selections.get_range_around_top_level_form()
+  delete_form_impl(selections.get_range_around_top_level_form())
+end
+
+local function delete_in_form_impl(range)
   if not range then
     return
   end
@@ -32,42 +38,16 @@ function M.delete_top_level_form()
     range[3], range[4],
     {}
   )
+
+  vim.api.nvim_win_set_cursor(0, { range[1] + 1, range[2] })
 end
 
 function M.delete_in_form()
-  local range = selections.get_range_in_form()
-  if not range then
-    return
-  end
-
-  local buf = vim.api.nvim_get_current_buf()
-  -- stylua: ignore
-  vim.api.nvim_buf_set_text(
-    buf,
-    range[1], range[2],
-    range[3], range[4],
-    {}
-  )
-
-  vim.api.nvim_win_set_cursor(0, { range[1] + 1, range[2] })
+  delete_in_form_impl(selections.get_range_in_form())
 end
 
 function M.delete_in_top_level_form()
-  local range = selections.get_range_in_top_level_form()
-  if not range then
-    return
-  end
-
-  local buf = vim.api.nvim_get_current_buf()
-  -- stylua: ignore
-  vim.api.nvim_buf_set_text(
-    buf,
-    range[1], range[2],
-    range[3], range[4],
-    {}
-  )
-
-  vim.api.nvim_win_set_cursor(0, { range[1] + 1, range[2] })
+  delete_in_form_impl(selections.get_range_in_top_level_form())
 end
 
 function M.delete_element()
