0f9420360c2ee4bd8116645032df04f5549f0033
Hoang Nguyen <folliekazetani@protonmail.com>
2025-05-25 11:54:36 +0700

    Update test workflow to use nvim-treesitter's main branch

--- tests/config.lua
+++ tests/config.lua
@@ -1,16 +1,14 @@
 vim.opt.runtimepath:append("./.build/dependencies/plenary.nvim")
 vim.opt.runtimepath:append("./.build/dependencies/nvim-treesitter")
-vim.opt.runtimepath:append("./.build/parsers")
 vim.opt.runtimepath:append(".")
 
 vim.cmd.runtime({ "plugin/plenary.vim", bang = true })
 vim.cmd.runtime({ "plugin/nvim-treesitter.lua", bang = true })
+vim.cmd.runtime({ "plugin/query_predicates.lua", bang = true })
+vim.cmd.runtime({ "plugin/filetypes.lua", bang = true })
 
 vim.o.swapfile = false
 vim.bo.swapfile = false
 
-require("nvim-treesitter.configs").setup({
-  parser_install_dir = vim.fn.getcwd() .. "/.build/parsers",
-  ensure_installed = { "clojure", "fennel", "scheme", "commonlisp" },
-  sync_install = true,
-})
+require("nvim-treesitter").setup({ install_dir = vim.fn.getcwd() .. "/.build/parsers" })
+require("nvim-treesitter.install").install({ "clojure", "fennel", "scheme", "commonlisp" }):wait()
